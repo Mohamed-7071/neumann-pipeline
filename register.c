7eb79e69ff1f0c2a97fdd456ec0b911c2f41de22
Ahmed-gamal9341 <ahmedgamalmahmoudd@gmail.com>
2025-05-15 15:17:01 +0300

    register file

---
+++ register.c
@@ -0,0 +1,36 @@
+#include "registers.h"
+#include <stdio.h>
+
+uint32_t registers[NUM_REGISTERS];
+uint32_t PC = 0;
+
+void init_registers() {
+    for (int i = 0; i < NUM_REGISTERS; i++) {
+        registers[i] = 0;
+    }
+    PC = 0;
+}
+
+uint32_t get_register(uint8_t index) {
+    if (index >= NUM_REGISTERS) {
+        fprintf(stderr, "Register index out of bounds: R%d\n", index);
+        return 0;
+    }
+    return registers[index];
+}
+
+void set_register(uint8_t index, uint32_t value) {
+    if (index == 0) return;  // R0 is hard-wired to zero
+    if (index >= NUM_REGISTERS) {
+        fprintf(stderr, "Register index out of bounds: R%d\n", index);
+        return;
+    }
+    registers[index] = value;
+}
+
+void print_registers() {
+    for (int i = 0; i < NUM_REGISTERS; i++) {
+        printf("R%-2d = %u\n", i, registers[i]);
+    }
+    printf("PC  = %u\n", PC);
+}
