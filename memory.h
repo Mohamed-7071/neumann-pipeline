f775c1f7008fd0074516e54637105e97a579031e
Your Name <mostafa.hegazy@student.guc.edu.eg>
2025-05-18 23:27:39 +0300

    .

--- memory.h
+++ memory.h
@@ -2,5 +2,11 @@
 #define MEMORY_H
 
 #define MEMORY_SIZE 2048
+#define MAX_INSTRUCTIONS 1024
+
+extern uint32_t memory[MEMORY_SIZE];
+extern uint32_t instruction_memory[MAX_INSTRUCTIONS];
+
+void init_memory(void);
 
 #endif // MEMORY_H
